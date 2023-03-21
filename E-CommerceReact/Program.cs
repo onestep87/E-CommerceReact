d090b870deb2328e68562e9fd0c3916a585cae2f
onestep87 <philip88088@gmail.com>
2023-03-21 10:22:15 +0100

    fix connection string

--- E-CommerceReact/Program.cs
+++ E-CommerceReact/Program.cs
@@ -4,6 +4,7 @@ using E_CommerceReact.Middleware;
 using E_CommerceReact.RequestHelpers;
 using E_CommerceReact.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -63,8 +64,9 @@ else
     var pgPass = pgUserPass.Split(":")[1];
     var pgHost = pgHostPort.Split(":")[0];
     var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
 
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+connString = $"Server ={ updatedHost}; Port ={ pgPort}; User Id = { pgUser }; Password ={ pgPass}; Database ={ pgDb};";
 }
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
